3d8f9eca5aa4d9cdcdec93ba86d21761c39aee28
JFoll0823 <justinfoll23@gmail.com>
2021-10-19 17:23:32 -0400

    Simple damage systems in place

---
+++ KingdomUnderSiege/Assets/Skeleton/Skeleton.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skeleton : MonoBehaviour
+{
+    float _healthPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _healthPoints = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == "Spear")
+        {
+            _healthPoints -= 1;
+        }
+
+        if (_healthPoints == 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
